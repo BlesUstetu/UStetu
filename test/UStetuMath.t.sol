494d32b05504d08957ff7f587aa575b440c5918a
BlesUstetu <lionustetu@gmail.com>
2026-08-25 11:31:21 +0700

    test: fix fee math tuple assertions

--- test/UStetuMath.t.sol
+++ test/UStetuMath.t.sol
@@ -7,28 +7,29 @@ import {UStetuMath} from "../contracts/libraries/UStetuMath.sol";
 contract UStetuMathTest is Test {
     function testFeeAtOnePercent() public pure {
         uint256 gross = 270e6;
-        uint256 fee = UStetuMath.calculateFee(gross, 100);
+        (uint256 fee, uint256 sellerProceeds) = UStetuMath.calculateFee(gross, 100);
         assertEq(fee, 2.7e6);
-        assertEq(gross - fee, 267.3e6);
+        assertEq(sellerProceeds, 267.3e6);
     }
 
     function testZeroFee() public pure {
-        assertEq(UStetuMath.calculateFee(1_000_000, 0), 0);
+        (uint256 fee, uint256 sellerProceeds) = UStetuMath.calculateFee(1_000_000, 0);
+        assertEq(fee, 0);
+        assertEq(sellerProceeds, 1_000_000);
     }
 
     function testRoundingDown() public pure {
         uint256 gross = 101;
-        uint256 fee = UStetuMath.calculateFee(gross, 100);
+        (uint256 fee, uint256 sellerProceeds) = UStetuMath.calculateFee(gross, 100);
         assertEq(fee, 1);
-        assertEq(gross - fee, 100);
+        assertEq(sellerProceeds, 100);
     }
 
     function testFuzzFeeConservation(uint256 gross, uint16 feeBps) public pure {
         feeBps = uint16(bound(feeBps, 0, 1_000));
         gross = bound(gross, 0, type(uint256).max / 1_000);
 
-        uint256 fee = UStetuMath.calculateFee(gross, feeBps);
-        uint256 sellerProceeds = gross - fee;
+        (uint256 fee, uint256 sellerProceeds) = UStetuMath.calculateFee(gross, feeBps);
 
         assertLe(fee, gross);
         assertEq(fee + sellerProceeds, gross);
