67f9ea3d093793d1dbea49bf4872fee16cbadf21
BlesUstetu <lionustetu@gmail.com>
2026-08-25 12:17:14 +0700

    test: cover token decimal normalization

--- test/UStetuMath.t.sol
+++ test/UStetuMath.t.sol
@@ -25,6 +25,24 @@ contract UStetuMathTest is Test {
         assertEq(sellerProceeds, 100);
     }
 
+    function testGrossPaymentNormalizes18To6Decimals() public pure {
+        uint256 tokenAmount = 100 ether;
+        uint256 unitPrice = 2_700_000;
+
+        uint256 grossPayment = UStetuMath.calculateGrossPayment(tokenAmount, unitPrice, 18);
+
+        assertEq(grossPayment, 270e6);
+    }
+
+    function testGrossPaymentSupportsFractionalTokenAmount() public pure {
+        uint256 tokenAmount = 100.5 ether;
+        uint256 unitPrice = 2_700_000;
+
+        uint256 grossPayment = UStetuMath.calculateGrossPayment(tokenAmount, unitPrice, 18);
+
+        assertEq(grossPayment, 271.35e6);
+    }
+
     function testFuzzFeeConservation(uint256 gross, uint16 feeBps) public pure {
         feeBps = uint16(bound(feeBps, 0, 1_000));
         gross = bound(gross, 0, type(uint256).max / 1_000);
