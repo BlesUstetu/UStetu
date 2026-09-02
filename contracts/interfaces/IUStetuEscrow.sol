8db907d1aea864bd09c4dbf936b44877f9b1ede7
BlesUstetu <lionustetu@gmail.com>
2026-09-02 23:34:50 +0700

    refactor: align escrow interface with trustless lifecycle

--- contracts/interfaces/IUStetuEscrow.sol
+++ contracts/interfaces/IUStetuEscrow.sol
@@ -4,16 +4,20 @@ pragma solidity 0.8.30;
 import {UStetuTypes} from "../libraries/UStetuTypes.sol";
 
 interface IUStetuEscrow {
+    function PAYMENT_WINDOW() external view returns (uint64);
+    function AUTO_RELEASE_WINDOW() external view returns (uint64);
+    function ORDER_EXPIRY() external view returns (uint64);
+
     function createOrder(uint256 listingId, uint256 tokenAmount) external returns (uint256 orderId);
     function fundOrder(uint256 orderId) external;
-    function releaseOrder(uint256 orderId) external;
-    function cancelOrder(uint256 orderId) external;
+    function completeOrder(uint256 orderId) external;
+    function autoReleaseOrder(uint256 orderId) external;
     function expireOrder(uint256 orderId) external;
-    function openDispute(uint256 orderId, uint8 reasonCode) external returns (uint256 disputeId);
-    function resolveDispute(uint256 disputeId, UStetuTypes.Resolution resolution) external;
-    function refundOrder(uint256 orderId) external;
-    function withdrawClaimable(address paymentToken, uint256 amount) external;
+
+    function withdrawClaimable(address token) external;
+
+    function getListing(uint256 listingId) external view returns (UStetuTypes.Listing memory);
     function getOrder(uint256 orderId) external view returns (UStetuTypes.Order memory);
-    function getDispute(uint256 disputeId) external view returns (UStetuTypes.Dispute memory);
-    function claimable(address paymentToken, address seller) external view returns (uint256);
+
+    function claimable(address account, address token) external view returns (uint256);
 }
