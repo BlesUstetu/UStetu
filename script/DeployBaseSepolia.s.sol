886e5191a1f8c9a134bc8f812bf321efcfeed4c2
BlesUstetu <lionustetu@gmail.com>
2026-09-03 10:31:55 +0700

    fix: import forge console2 in Base Sepolia script

--- script/DeployBaseSepolia.s.sol
+++ script/DeployBaseSepolia.s.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.30;
 
 import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
 import {UStetuRegistry} from "../contracts/core/UStetuRegistry.sol";
 import {UStetuSellerRegistry} from "../contracts/core/UStetuSellerRegistry.sol";
 import {UStetuEscrow} from "../contracts/core/UStetuEscrow.sol";
