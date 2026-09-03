3de5f6da9505163630f7a29aeb9b97dd767f09c5
BlesUstetu <lionustetu@gmail.com>
2026-09-03 10:20:11 +0700

    feat: expose configurable escrow fee in interface

--- contracts/interfaces/IUStetuEscrow.sol
+++ contracts/interfaces/IUStetuEscrow.sol
@@ -7,6 +7,13 @@ interface IUStetuEscrow {
     function PAYMENT_WINDOW() external view returns (uint64);
     function AUTO_RELEASE_WINDOW() external view returns (uint64);
     function ORDER_EXPIRY() external view returns (uint64);
+    function BPS_DENOMINATOR() external view returns (uint256);
+    function DEFAULT_FEE_BPS() external view returns (uint256);
+    function MIN_FEE_BPS() external view returns (uint256);
+    function MAX_FEE_BPS() external view returns (uint256);
+    function feeBps() external view returns (uint256);
+    function owner() external view returns (address);
+    function setFeeBps(uint256 newFeeBps) external;
 
     function createOrder(uint256 listingId, uint256 tokenAmount) external returns (uint256 orderId);
     function fundOrder(uint256 orderId) external;
