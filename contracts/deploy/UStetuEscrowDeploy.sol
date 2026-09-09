9cf66333dfd1675d48056c1d67caae54714540fe
BlesUstetu <lionustetu@gmail.com>
2026-09-09 21:00:45 +0700

    deploy: update escrow deployment wrapper for seller registry

--- contracts/deploy/UStetuEscrowDeploy.sol
+++ contracts/deploy/UStetuEscrowDeploy.sol
@@ -6,7 +6,7 @@ import {UStetuEscrow} from "../core/UStetuEscrow.sol";
 /// @notice Dedicated Remix deployment entrypoint for UStetuEscrow.
 /// @dev Inherits the production escrow without changing any escrow logic.
 contract UStetuEscrowDeploy is UStetuEscrow {
-    constructor(address registryAddress, address feeRecipientAddress)
-        UStetuEscrow(registryAddress, feeRecipientAddress)
+    constructor(address registryAddress, address sellerRegistryAddress, address feeRecipientAddress)
+        UStetuEscrow(registryAddress, sellerRegistryAddress, feeRecipientAddress)
     {}
 }
