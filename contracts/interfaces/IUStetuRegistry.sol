9a9edeaa36709b1e8b80fc585cb65f4e77e79833
BlesUstetu <lionustetu@gmail.com>
2026-09-18 15:03:38 +0700

    refactor: align registry interface with v1 permissionless model

--- contracts/interfaces/IUStetuRegistry.sol
+++ contracts/interfaces/IUStetuRegistry.sol
@@ -4,8 +4,10 @@ pragma solidity 0.8.30;
 import {UStetuTypes} from "../libraries/UStetuTypes.sol";
 
 interface IUStetuRegistry {
+    function registerToken(uint256 chainId, address token) external returns (bytes32 tokenId);
     function getToken(bytes32 tokenId) external view returns (UStetuTypes.Token memory);
-    function isApprovedToken(bytes32 tokenId) external view returns (bool);
-    function isSupportedPaymentToken(address token) external view returns (bool);
+    function isRegisteredToken(bytes32 tokenId) external view returns (bool);
+    function getPaymentToken() external view returns (address);
     function getDeploymentChainId() external view returns (uint256);
+    function getTokenId(uint256 chainId, address token) external pure returns (bytes32);
 }
